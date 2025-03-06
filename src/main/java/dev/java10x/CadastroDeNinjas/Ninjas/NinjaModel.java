942f66fd1dde1a721228c146962423d6cccbc793
Andrei Gravonski <gravonskia@gmail.com>
2025-03-05 22:52:05 -0300

    Introdução a Lombok

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -3,6 +3,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 import jdk.dynalink.linker.LinkerServices;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.mapping.Join;
 
 import java.util.List;
@@ -10,6 +13,9 @@ import java.util.List;
 //Entity: Transforma uma classe comum em uma entidade no BD
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -26,33 +32,4 @@ public class NinjaModel {
     public NinjaModel() {
     }
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
