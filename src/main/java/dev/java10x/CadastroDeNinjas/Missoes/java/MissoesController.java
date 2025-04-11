bf15d2de17d6764e85837c5e553e83682679de1c
Andrei Gravonski <gravonskia@gmail.com>
2025-04-11 00:34:29 -0300

    feat: adiciona a classe MissoesControllerUI para a conexão visual com usuario, além da adição do Response Entity e Thymeleaf na aplicação

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/java/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/java/MissoesController.java
@@ -1,72 +1,14 @@
 package dev.java10x.CadastroDeNinjas.Missoes.java;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/missoes")
-public class MissoesController {
-
-    //Injeção de Dependência
-
-    private final MissoesService missoesService;
-
-
-    public MissoesController(MissoesService missoesService) {
-        this.missoesService = missoesService;
-    }
-
-    //Criação das Missões
-    @PostMapping("/criar")
-    public ResponseEntity<String> missoesNinja(@RequestBody MissoesDTO missoes){
-        MissoesDTO novaMissao = missoesService.criarMissoes(missoes);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Missão criada com sucesso!");
-    }
-
-    //Listagem das Missões
-    @GetMapping("/listar")
-    public ResponseEntity<List<MissoesDTO>> listarMissoes() {
-        List<MissoesDTO> missoes = missoesService.listarMissoes();
-        return ResponseEntity.ok(missoes);
-    }
-
-    //Listagem de Ninjas por ID
-    @GetMapping("/listar/{id}")
-    public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id){
-        MissoesDTO missoes = missoesService.buscarMissoesPorId(id);
-        if(missoes != null){
-            return ResponseEntity.ok(missoes);
-        } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("Missão com ID " + id + " não encontrada!");
-        }
-    }
-
-    //Alteração das Missões por ID
-    @PutMapping("/alterar/{id}")
-    public String alteraMissoes( @PathVariable Long id, @RequestBody MissoesDTO missoes){
-        MissoesDTO missaoAlterada = missoesService.atualizarMissoes(id, missoes);
-        if(missaoAlterada != null){
-            return missaoAlterada.toString();
-        } else {
-            return null;
-        }
-
-    }
-
-    //Deletar Missões por ID
-    @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<String> deletarMissoes (@PathVariable Long id){
-        MissoesDTO missoes = missoesService.buscarMissoesPorId(id);
-        if(missoes != null){
-            return ResponseEntity.ok("Missão deletada com sucesso!");
-        }else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("Missão com ID " + id + " não encontrada!");
-        }
-    }
+//Deletar Missões por ID
+@DeleteMapping("deletar/{id}")
+public ResponseEntity<String> deletarMissoes (@PathVariable Long id) {
+    if (deletarMissoes())
 }
+
+
+
+
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/java/MissoesControllerUi.java
@@ -0,0 +1,4 @@
+package dev.java10x.CadastroDeNinjas.Missoes.java;
+
+public class MissoesControllerUi {
+}
